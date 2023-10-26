88c7ec92c8b33f94d19a42bc62a53e1f1d751b74
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2023-10-26 18:39:09 +0300

    Added task class for tasks.js

---
+++ src/tasks.js
@@ -0,0 +1,13 @@
+//retrieve data from html forms, add hide and show for the task button, update the task with form data.  
+
+class task {
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    };
+}
+function taskGenerate() {
+
+}

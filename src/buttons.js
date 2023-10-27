78c6b4640de506240a1f942b0b4dd4293fffd123
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2023-10-27 21:18:56 +0300

    Added dialogs for project and task detail page

--- src/buttons.js
+++ src/buttons.js
@@ -1,14 +1,27 @@
 //manage buttons functionality
 
 const taskBtn = document.querySelector("#task-button");
+const taskDialog = document.querySelector("#task-dialog");
+const cancelTask = document.querySelector("#cancel-task");
+
 const projectBtn = document.querySelector("#project-button");
+const projectDialog = document.querySelector("#project-dialog");
+const cancelProj = document.querySelector("#cancel-project");
 
 export default function buttonFuncs(){
     taskBtn.addEventListener("click", () =>{
-        
-    })
-    
+        taskDialog.showModal();
+    });
+
+    cancelTask.addEventListener("click", () => {
+        taskDialog.close();
+    });
+
     projectBtn.addEventListener("click", () =>{
-        
-    })
+        projectDialog.showModal();
+    });
+
+    cancelProj.addEventListener("click", () => {
+        projectDialog.close();
+    });
 }
